f41ed1c3a34c1ddf90d9d78c7448c6e194c81afd
Julia Band <jbandyaeva@gmail.com>
2020-03-02 23:38:29 +0300

    Добавьте файлы проекта.

---
+++ MatrixView/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MatrixView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Follow the White Rabbit.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadKey();
+        }
+    }
+}
